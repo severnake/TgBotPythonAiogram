e8faf4da67b3e0d33819b00a9ed8629190e6d587
vladneverov <neverovprod@gmail.com>
2020-04-27 19:48:08 +0300

    Telegram Bot with Aiogram

---
+++ bot.py
@@ -0,0 +1,21 @@
+from config import TOKEN
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_hello(message: types.Message):
+      await bot.send_message(message.from_user.id, 'Привет\nЧто будем делать?')
+
+@dp.message_handler(commands=['help'])
+async def process_help(message: types.Message):
+      await bot.send_message(message.from_user.id, 'Нужна помощь?')
+
+@dp.message_handler()
+async def process_reply(message: types.Message):
+      await message.reply('Ваш текст сообщения!!!')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
